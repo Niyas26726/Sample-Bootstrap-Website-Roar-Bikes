2cf3cff9d7e9d7a2c5560cb28c602bbebd227aea
Niyas <pnmohamedniyas26726@gmail.com>
2024-09-25 15:54:00 +0530

    First Commit

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_POST['name'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+}
+if (isset($_POST['email'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+}
+
+// Define subject and recipient
+$subject = "Form Submission";
+$recipient = "daniel@byol.ie";
+$content = "From: $name";
+$mailheader = "From: $email \r\n";
+
+if (mail($recipient, $subject, $content, $mailheader)) {
+    echo "Email sent!";
+} else {
+    echo "Failed to send email.";
+}
+?>
